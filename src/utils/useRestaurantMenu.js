bb9018569f4faea0e56020c12a60bd6198b4199e
savioursubham <savioursubham@gmail.com>
2024-02-05 00:40:06 +0530

    cors-fetch added for restaurant menu

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MENU_URL } from "./constant";
+import { PROXY_URL } from "./mockData";
 
 const useRestaurantMenu = (resId) => {
     const [resInfo,setResInfo] = useState(null);
@@ -8,7 +9,7 @@ const useRestaurantMenu = (resId) => {
     },[]);
 
     const fetchData = async () => {
-        const data = await fetch(MENU_URL+resId+'&catalog_qa=undefined&submitAction=ENTER');
+        const data = await fetch(PROXY_URL + MENU_URL+resId+'&catalog_qa=undefined&submitAction=ENTER');
         const json = await data.json();
 
         setResInfo(json.data);
